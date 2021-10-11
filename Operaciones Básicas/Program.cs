3af7faf584b4acdbac1918a16c8108c1fe19a027
cantl <cantl@DESKTOP-M6FBNG3>
2021-10-11 02:28:18 -0500

    Agregar archivos de proyecto.

---
+++ Operaciones Básicas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperacionesBasicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double res;
+            Console.WriteLine("Introduce el Primer numero:");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Introduce el Segundo numero: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+            res = numero1 + numero2;
+            Console.WriteLine("Resultado de la Suma: " + res);
+            res = numero1 - numero2;
+            Console.WriteLine("Resultado de la Resta: " + res);
+            res = numero1 / numero2;
+            Console.WriteLine("Resultado de la Division: " + res);
+            res = numero1 * numero2;
+            Console.WriteLine("Resultado de la Multiplicacion: " + res);
+            Console.ReadKey();
+        }
+    }
+}
